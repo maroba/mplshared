c0dd4b0bbe190e8a2993daf4871aa8d264c0c311
Matthias Baer <matthias.baer@mpl.mpg.de>
2022-09-14 14:35:37 +0200

    Refactor

--- mplshared/step.py
+++ mplshared/step.py
@@ -129,39 +129,32 @@ class StepFunctionLine(matplotlib.lines.Line2D):
         return self.stepfunc(x)
 
     def _convert_data(self, stepfunc, baseline):
-        x = stepfunc.x
-        y = stepfunc.y
         xdata = []
         ydata = []
 
         idx = 0
         first_idx = 0
-        last_idx = len(x) - 1
-        for x_, y_ in zip(x, y):
+        last_idx = len(stepfunc.x) - 1
+        for x_, y_, bin_ in zip(stepfunc.x, stepfunc.y, stepfunc.bins):
             if idx == first_idx:
-                dx = x[1] - x[0]
-                xdata.append(x[0] - dx / 2)
+                xdata.append(bin_[0])
                 ydata.append(baseline)
-                xdata.append(x[0] - dx / 2)
-                ydata.append(y[0])
-                xdata.append(x[0] + dx / 2)
-                ydata.append(y[0])
+                xdata.append(bin_[0])
+                ydata.append(y_)
+                xdata.append(bin_[1])
+                ydata.append(y_)
             elif idx == last_idx:
-                dx = x[-1] - x[-2]
-                xdata.append(x[-1] - dx / 2)
-                ydata.append(y[-1])
-                xdata.append(x[-1] + dx / 2)
-                ydata.append(y[-1])
-                xdata.append(x[-1] + dx / 2)
+                xdata.append(bin_[0])
+                ydata.append(y_)
+                xdata.append(bin_[1])
+                ydata.append(y_)
+                xdata.append(bin_[1])
                 ydata.append(baseline)
             else:
-                x_left = x[idx - 1] + 0.5 * (x[idx] - x[idx - 1])
-                x_right = x[idx] + 0.5 * (x[idx + 1] - x[idx])
-                xdata.append(x_left)
-                xdata.append(x_right)
-                ydata.append(y[idx])
-                ydata.append(y[idx])
-
+                xdata.append(bin_[0])
+                xdata.append(bin_[1])
+                ydata.append(y_)
+                ydata.append(y_)
             idx += 1
 
         return xdata, ydata
